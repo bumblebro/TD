08c085b65f19fe0350f0c460997c2e8ddeb8d046
ShreyasMS <shreyasmadappady20@gmail.com>
2025-05-18 11:29:53 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,9 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/", "/admin/", "/watch"],
+    //  disallow: ["/api/", "/admin/", "/watch"],
+     disallow: ["/api/", "/admin/"],
+
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   };
