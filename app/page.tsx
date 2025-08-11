80511d6dee5986f8ba57b38180c7aab329af5536
Shreyas M S <shreyasmadappady20@gmail.com>
2025-08-11 15:27:46 +0530

    c

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+
 import Link from "next/link";
 import { useEffect, useState, useCallback } from "react";
 import { useRouter } from "next/navigation";
