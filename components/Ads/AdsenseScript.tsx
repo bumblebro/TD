e2f40cea8dcd0ab7192190ece985177c0935a0c8
Shreyas M S <shreyasmadappady20@gmail.com>
2025-08-11 23:27:43 +0530

    c

--- components/Ads/AdsenseScript.tsx
+++ components/Ads/AdsenseScript.tsx
@@ -10,7 +10,7 @@ export default function AdsenseScript() {
       async
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5012580427673167"
       crossOrigin="anonymous"
-      strategy="afterInteractive"
+      strategy="lazyOnload"
     />
   );
 }
