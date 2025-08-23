9f1d0a2d5993c1487b641d69d2c8fec2ae6162a3
Shreyas M S <shreyasmadappady20@gmail.com>
2025-08-23 23:42:03 +0530

    c

--- app/page.tsx
+++ app/page.tsx
@@ -265,7 +265,7 @@ export default function Home() {
                           ? `/watch?token=${encodeURIComponent(url)}`
                           : undefined
                       }
-                      onClick={handleSubmit}
+                      // onClick={handleSubmit}
                       className={`w-full block text-center px-4 py-2 text-sm font-medium text-white transition-colors duration-200 bg-blue-500 rounded-lg sm:py-3 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:text-base${
                         !url
                           ? " opacity-50 cursor-not-allowed pointer-events-none"
